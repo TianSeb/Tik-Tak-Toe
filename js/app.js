61fcb83b1c342da4ba91f820bb6723581b49a18e
TianSeb <sebastianalasia86@gmail.com>
2021-06-30 14:30:48 -0300

    second version

--- js/app.js
+++
@@ -1,4 +0,0 @@
-import { events } from "Controller/eventsControl.js";
-import { gameBoard } from "gameFiles/gameBoard.js";
-import { matchFlow, checkResult } from "gameFiles/gameFlow.js";
-import { iaMove } from "gameFiles/ia.js";
